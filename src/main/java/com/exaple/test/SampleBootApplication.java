4b6de1287a7bd3a8129ed9e5b26aa70b7eb9023f
vagi <vagi@192.168.1.15>
2017-06-25 18:13:46 +0900

    first commit

---
+++ src/main/java/com/exaple/test/SampleBootApplication.java
@@ -0,0 +1,26 @@
+package com.exaple.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@RestController
+public class SampleBootApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SampleBootApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
