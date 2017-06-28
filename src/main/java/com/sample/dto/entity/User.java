c90e85a7fbc0284af6cca653861dfc7308363fe7
vagi <vagi@192.168.1.15>
2017-06-29 08:15:52 +0900

    add User.java

---
+++ src/main/java/com/sample/dto/entity/User.java
@@ -0,0 +1,22 @@
+package com.sample.dto.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstName;
+    private String lastName;
+}
